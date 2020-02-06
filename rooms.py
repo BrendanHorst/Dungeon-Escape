9081f1e70f31eab2a109f079cba0c4d003d18722
Brendan Horst <yoshistar2000@gmail.com>
2020-02-06 09:46:57 -0500

    Added text and functionality to almost all of the rooms

--- rooms.py
+++ rooms.py
@@ -22,11 +22,17 @@ class Start(Room):
 
         self.adjacent_rooms['north'] = 'dungeon'
 
-        print("The cell is empty aside ")
+        print("\nYou look around inside the dilapidated cell.  The bars are rusted and bent all out of shape, and the door is nonexistant.  Up above you can see the hole you fell down; it's so narrow and tall that you can't see the top.  Other than the remains of a wooden bench there isn't anything else in the cell.")
 
-        input()
+        while True:
 
-        return self.move('north')
+            print("What will you do?")
+            print("1: Leave the cell")
+
+            decision = input(player.prompt)
+
+            if decision == '1':
+                return self.move('north')
 
 
 class Dungeon(Room):
@@ -37,6 +43,8 @@ class Dungeon(Room):
         self.adjacent_rooms['north'] = 'north_cell'
         self.adjacent_rooms['east'] = 'east_cell'
         self.adjacent_rooms['west'] = 'crossroads'
+
+        print("\nYou enter what appears to be the ruins of an ancient dungeon, with rubble everywhere.  There are only 3 cells accessible; anything else seems to have been cut off from a cave-in")
 
         while True:
 
@@ -50,9 +58,10 @@ class Dungeon(Room):
                 return self.move('south')
             elif direction == '3':
                 if player.jail_key == True:
+                    print("With the key in had, the cell door opens without a hitch.")
                     return self.move('east')
                 else:
-                    print("You attempt to open the cell door, but it is locked tight!")
+                    print("You attempt to open the east cell door, but it is locked tight!")
             elif direction == '4':
                 return self.move('west')
 
@@ -63,7 +72,17 @@ class North_Cell(Room):
 
         self.adjacent_rooms['south'] = 'dungeon'
 
-        return self.move('south')
+        print("\nYou enter the north cell.")
+
+        while True:
+
+            print("What will you do?")
+            print("1: Leave the Cell")
+
+            decision = input(player.prompt)
+
+            if decision == '1':
+                return self.move('south')
 
 
 class East_Cell(Room):
@@ -72,7 +91,28 @@ class East_Cell(Room):
 
         self.adjacent_rooms['west'] = 'dungeon'
 
-        return self.move('west')
+        print("\nYou enter the east cell.  There is a mysterious package hidden underneath the wooden bench.")
+
+        while True:
+
+            print("What will you do?")
+            print("1: Leave the Cell")
+            print("2: Open the package")
+
+            decision = input(player.prompt)
+
+            if decision == '1':
+                return self.move('west')
+            if decision == '2':
+                if player.jetpack == False:
+
+                    print("You carefully open up the package.  Inside it is a jetpack!  What luck!  It appears to be pretty old and fragile, but it could definitely help out.")
+                    player.jetpack = True
+
+                elif player.jetpack == True:
+
+                    print("Unsatisfied with the jetpack, you reach back into the package for anything else, only to be greeted with an intense, sharp pain.  Inside the package is a strange looking scorpion, but before you can process it your whole body disintegrates into dust.")
+                    #death
 
 
 # -----Cavern Area-----
@@ -84,6 +124,8 @@ class Crossroads(Room):
         self.adjacent_rooms['east'] = 'dungeon'
         self.adjacent_rooms['west'] = 'bridge_east'
         self.adjacent_rooms['north'] = 'root_forest'
+
+        print("\nYou enter a large open cavern with crumbling stone brick paths showing the way.")
 
         while True:
 
@@ -106,7 +148,7 @@ class Bridge_East(Room):
         self.adjacent_rooms['east'] = 'crossroads'
         self.adjacent_rooms['west'] = 'bridge_west'
 
-        print("Standing before you is what seems to have once been a bridge, however all that remains is an impassable ravine stretching down into the abyss.")
+        print("\nStanding before you is what seems to have once been a bridge, however all that remains is an impassable ravine stretching down into the abyss.")
 
         while True:
 
@@ -118,13 +160,26 @@ class Bridge_East(Room):
             decision = input(player.prompt)
 
             if decision == '1':
+
                 print("You pick up the nearest stone and drop it over the edge.  The rock is swallowed by the darkness and never makes a sound.  Strangely, the air around you feels a bit colder and you feel a slight sense of dread.")
                 player.bad_karma = True
+
             elif decision == '2' and player.jetpack == False:
+
                 print("I said the ravine was impassable, did I not?  You fall and die.  You idiot.")
                 #death
+
             elif decision == '2' and player.jetpack == True:
-                pass
+
+                print("You make a flying leap over the edge, and the jetpack kicks in and lifts you up.")
+
+                if player.bad_karma == True:
+
+                    print("As you fly across, you feel the same sense of dread that you felt when throwing the stone down.  It feels as if... something is angry.  Suddenly, a mass of shadow rises up from the depths, and a tendril reached out and grabs you by the ankle.  It pulls you down into the abyss, but you were already dead the moment it touched you.")
+                    #death
+
+                print("You fly across safely, and you can't help but let out a sigh of releif as your feet alight upon the other side.")
+
             elif decision == '3':
                 return self.move('east')
 
@@ -133,6 +188,24 @@ class Bridge_West(Room):
     def enter(self):
 
         self.adjacent_rooms['east'] = 'bridge_east'
+        self.adjacent_rooms['north'] = 'cavern_entrance'
+
+        print("\nYou are on the western side of the destroyed bridge.")
+
+        while True:
+
+            print("Where will you go?")
+            print("1: Head north")
+            print("2: Fly across the ravine (east)")
+
+            decision = input(player.prompt)
+
+            if decision == '1':
+                return self.move('north')
+            if decision == '2':
+
+                print("You fly back across the ravine using the jetpack")
+                return self.move('east')
 
 class Cavern_Entrance(Room):
 
@@ -152,6 +225,37 @@ class Root_Forest(Room):
         self.adjacent_rooms['south'] = 'crossroads'
         self.adjacent_rooms['west'] = 'waterfall'
 
+        print("\nYou enter a room filled with the roots from what must be a forest on the surface, so that it's hard to move around.")
+
+        while True:
+
+            print("What will you do?")
+            print("1: Head north")
+            print("2: Head south")
+            print("3: Head east")
+            print("4: Head west")
+
+            decision = input(player.prompt)
+
+            if decision == '1':
+
+                print("You try to head north, and see a small passageway hidden behind the roots.  You work your way over to it only to see a pair of glowing eyes looking out the darkness back at you.  Before you have time to react, whatever it was grabs you and pulls you into its den.")
+                #death
+
+            if decision == '2':
+                return self.move('south')
+            if decision == '3':
+
+                print("You try to head east through the roots, only to hit a wall.")
+
+                if player.jail_key == False:
+                    print("However, you see a small glimmer on the ground.  You push some roots aside and grab what turns out to be a slightly rusted key.  This could come in handy.")
+                    print("You obtained the rusty key!")
+                    player.jail_key = True
+
+            if decision == '4':
+                return self.move('west')
+
 
 class Waterfall(Room):
 
@@ -159,12 +263,48 @@ class Waterfall(Room):
 
         self.adjacent_rooms['east'] = 'root_forest'
 
+        print("You hit a dead end in a small room with a waterfall pouring into a small pool of clear water")
+
+        drink_count = 0;
+
+        while True:
+
+            print("What do you do?")
+            print("1: Stop to take a drink")
+            print("2: Head east")
+
+            decision = input(player.prompt)
+
+            if drink_count == 2:
+                 decision = '1'
+
+            if decision == '1':
+
+                if drink_count == 0:
+
+                    print("You decide to take a drink from the water.  It's delicious.  You feel compelled to drink it agian.")
+                    drink_count = 1
+
+                if drink_count == 1:
+
+                    print("The water is so good you gulp it down.  The water is cool.  The water is refreshing.  The water is all you need.")
+                    drink_count = 2
+
+                if drink_count ==2:
+
+                    print("You can't even make decisions anymore, all you can think of is drinking more water from the pool.  Suddenly, a beautiful water spirit materializes before your eyes and reaches her hand out to you.  Without thinking, you take it, and she abruptly pulls you under the water, which is a lot deeper than it looks from above.  You try to tell your body to wrestle free, but you've been cursed by the water and are completely helpless.  You accept your fate and drown.")
+                    #death
+
+            if decision == '2':
+
+                return self.move('east')
+
 
 
 class Goal(Room):
 
     def enter(self):
 
-        print("You escaped!")
+        print("Congradulations, You escaped!")
 
         return('win')
